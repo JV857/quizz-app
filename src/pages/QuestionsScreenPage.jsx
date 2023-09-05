25744400f02044e0b25da7ef597cfcde1e3cc1eb
javed-hfn <javed.a@volunteer.heartfulness.org>
2023-09-05 21:52:58 +0530

    1st commit

---
+++ src/pages/QuestionsScreenPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Questions from '../components/Questions'
+
+const QuestionsScreenPage = () => {
+  return (
+    <div><Questions/></div>
+  )
+}
+
+export default QuestionsScreenPage
